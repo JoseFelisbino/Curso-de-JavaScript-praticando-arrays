4117222bece0770d21e9d3869064387fb12ef11f
JoseFelisbino <lu31660@gmail.com>
2026-04-21 11:49:30 -0300

    feat: adiciona resolução do exercício

---
+++ main.js
@@ -0,0 +1,7 @@
+const pedidos = ['camiseta', 'calça', 'tênis'];
+
+const novoPedidos = [...pedidos, 'boné'];
+
+console.log("Array original: ", pedidos);
+console.log("Array copiado: ", novoPedidos);
+
